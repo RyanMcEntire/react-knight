f7582e30fcbcd3cbd623dec8622f1d1da259bbb6
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-10-19 12:35:18 -0600

    increase jump height

--- src/constants/gameData.ts
+++ src/constants/gameData.ts
@@ -4,6 +4,6 @@ export const playerHeight = 246;
 export const latMovementSpeed = 250;
 export const baseGravity = 9.8 * 90;
 export const megaGravity = baseGravity * 2.5;
-export const jumpVelocity = -360;
+export const jumpVelocity = -380;
 export const scale = 2;
 export const playerScale = 1
